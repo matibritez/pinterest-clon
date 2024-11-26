bb33175b46d368e0817e8cb4049676706d562f1a
unknown <matiasbritez36@gmail.com>
2024-11-25 21:07:37 -0300

    primer-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header  from './components/Header'
+
+function App() {
+  return (
+    <div className="container">
+      <Header/>
+      new project
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <ul>
+            <li><a href=''>Logo</a></li>
+            <li><a href=''>Inicio</a></li>
+            <li><a href=''>Hoy</a></li>
+            <li><a href=''>Crear</a></li>
+            <li><input type='search'></input></li>
+            <li><a href=''>User</a></li>
+        </ul>
+    </header>
+  );
+}
+
+export default Header;
